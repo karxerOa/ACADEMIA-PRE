80e78bd2a36a07c00022345202273645b4d6ea40
Stephany Jaseydy Lezama Ninatanta <N00383303@upn.pe>
2025-07-08 16:52:20 -0500

    Se agregó la interfaz DetalleCurso

---
+++ ACADEMIA-PRE/DetalleCurso.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ACADEMIA_PRE
+{
+    public partial class DetalleCurso : Form
+    {
+        public DetalleCurso(string idCurso, string denominacion)
+        {
+            InitializeComponent();
+        }
+
+        private void DetalleCurso_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
